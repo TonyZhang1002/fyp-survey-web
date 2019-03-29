ae426972faed3e472651c826efc2da5d0239fa9f
TonyZhang1002 <z496722204a@hotmail.com>
2019-03-29 09:52:55 +0000

    Initial commit

---
+++ src/main/java/com/example/fypsurveyweb/FypSurveyWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.fypsurveyweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FypSurveyWebApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(FypSurveyWebApplication.class, args);
+   }
+
+}
