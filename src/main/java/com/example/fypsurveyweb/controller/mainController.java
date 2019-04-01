836aab760ec6148d6d957f10726e09198ee5205a
TonyZhang1002 <z496722204a@hotmail.com>
2019-04-01 09:56:07 +0100

    Two pic added

--- src/main/java/com/example/fypsurveyweb/controller/mainController.java
+++ src/main/java/com/example/fypsurveyweb/controller/mainController.java
@@ -12,15 +12,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class mainController {
    @GetMapping("")
    public String formPage(Model model) {
-      int randomImg = (int) (Math.random() * 17);
-      int randomTag = (int) (Math.random() * 4);
-      switch (randomTag) {
-         case 0:  model.addAttribute("imgName", randomImg + "-ord.jpg");
-         case 1:  model.addAttribute("imgName", randomImg + "-ori.jpg");
-         case 2:  model.addAttribute("imgName", randomImg + "-gan.png");
-         case 3:  model.addAttribute("imgName", randomImg + "-res.png");
-         default:
+
+      for (int i = 1; i <= 2; i++) {
+         int randomImg = (int) (Math.random() * 17);
+         int randomTag = (int) (Math.random() * 4);
+         switch (randomTag) {
+            case 0:  model.addAttribute("imgName" + i + "0", randomImg + "-ord.jpg");
+            case 1:  model.addAttribute("imgName" + i + "0", randomImg + "-ori.jpg");
+            case 2:  model.addAttribute("imgName" + i + "0", randomImg + "-gan.png");
+            case 3:  model.addAttribute("imgName" + i + "0", randomImg + "-res.png");
+            default:
+         }
       }
+
       return "index";
    }
 }
